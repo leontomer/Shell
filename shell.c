ba7dac4f178e0850aa88b8b9dd980f76886d97ef
tomer <leontomer@gmail.com>
2020-04-11 15:59:54 -0700

    added cp

--- shell.c
+++ shell.c
@@ -7,27 +7,32 @@
 
  int main (int argc, char **argv)
  {
+     
 int childPid;
 char * cmdLine;
 char *info;
+//char file2[256];
+char file1[256];
 while (1){
+    
     cmdLine= readline(">"); 
 //printf("%s",cmdLine);
 //info = parse(cmdLine);
-//***important: need to write a function to get the first word(which is-the command) from cmdline
-char a[5]="/bin/";
-info=strcat(a,cmdLine);
+
+//char a[5]="/bin/";
+//info=strcat(a,cmdLine);
 //printf("%s",info);
 childPid = fork();
-char *av[]={cmdLine,*argv,NULL};
-
+//har *av[]={cmdLine,*argv,NULL};
+  //char * nw=(char*)malloc(sizeof(char));
+ 
 if (childPid == 0)
 {
    char * cpyCmd;
    strcpy(cpyCmd,cmdLine);
   char * word=strtok(cpyCmd, " ");
   
-//  printf("%s",cmdLine);
+ //printf("%s",word);
     if (strcmp(cmdLine, "pwd") == 0) {
         
     char* av[]={"pwd",NULL};
@@ -40,6 +45,7 @@ if (childPid == 0)
    // printf("%s",cmdLine);
     //printf("%s",word);
         chdir(word);
+
        // exit(0);
     }
     else if(strcmp(word,"nano")==0){
@@ -47,6 +53,7 @@ if (childPid == 0)
     word=  strtok(NULL, " ");
   //  printf("%s",cmdLine);
         creat(word,  0644);
+
         //printf("%s",strtok(cmdLine,"nano ")); 
         //printf("%s", cmdLine);
     }
@@ -54,8 +61,10 @@ if (childPid == 0)
           //                char* word = strtok(cmdLine, " ");
     word = strtok(NULL, " ");
     word = strtok(NULL, " ");
+
     //printf("%s",word);
        creat(word,  0644);  
+
       // printf("%s",strtok(cmdLine,"cat > ")); 
        //printf("%s", cmdLine);
     }
@@ -64,12 +73,30 @@ else if(strcmp(word,"cat")==0)
 
     //char* word = strtok(cmdLine, " ");
     word = strtok(NULL, " ");
+    
     execl("/bin/cat", "cat", word, NULL);
+ 
+}
+
+
+
+else if(strcmp(word,"cp")==0){
+    printf("hi");
+    word=strtok(NULL, " ");
+    //printf("%s",word);
+    strcpy(file1,word);
+     word=strtok(NULL, " ");
+  //  printf("%s",nw);
+   //  strcpy(file2,word);
+     //file2=strtok(NULL, " ");
+      execl("/bin/cp", "cp", file1,word, NULL);
 }
 
 else if(strcmp(cmdLine, "exit") == 0){
+
     kill(0,1);
 }
+
    else{
        printf("\'%s\' command is not supported\n",cmdLine);
        exit(0);
