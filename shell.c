7766063e21e615d21ef52bc2f61299184fbe52e9
Alex <alex.kreinis@walla.co.il>
2020-04-13 19:04:18 +0300

    HEY

--- shell.c
+++ shell.c
@@ -1,6 +1,6 @@
 
 #include <stdio.h>
-#include<unistd.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -14,16 +14,10 @@
 
     char file1[256];
      while (1){
-    
+            printf("\nENTER YOUR COMMAND: \n");
             cmdLine= readline(">"); 
-            //printf("%s",cmdLine);
-            //info = parse(cmdLine);
-            //char a[5]="/bin/";
-            //info=strcat(a,cmdLine);
-            //printf("%s",info);
             childPid = fork();
-            //har *av[]={cmdLine,*argv,NULL};
-            //char * nw=(char*)malloc(sizeof(char));
+       
  
             if (childPid == 0){
         
@@ -31,45 +25,37 @@
               strcpy(cpyCmd,cmdLine);
               char * word=strtok(cpyCmd, " ");
   
-              //printf("%s",word);
+
                 if (strcmp(cmdLine, "pwd") == 0) {
         
                     char* av[]={"pwd",NULL};
                     execvp("pwd",av);}
        
                 else if(strcmp(word,"cd")==0) {
-                    //printf("%s",cmdLine);
-                    //        char* word = strtok(cmdLine, " ");
                     word = strtok(NULL, " ");
-                    // printf("%s",cmdLine);
-                    //printf("%s",word);
                     chdir(word);
-                    // exit(0);
+                    }
+
+                 else if(strcmp(word,"grep")==0) {
+                        printf("entered here\n");
+                        word = strtok(NULL, " ");
+                        strcpy(file1,word);
+                        word = strtok(NULL, " ");
+                        execlp("grep","grep",file1,word,NULL);
                     }
                 else if(strcmp(word,"nano")==0){
-                     // char* word = strtok(cmdLine, " ");
                     word=  strtok(NULL, " ");
-                    //  printf("%s",cmdLine);
                      creat(word,  0644);
-                    //printf("%s",strtok(cmdLine,"nano ")); 
-                    //printf("%s", cmdLine);
                  }
                 else if(strstr(cmdLine,"cat >")){
-                    //char* word = strtok(cmdLine, " ");
                     word = strtok(NULL, " ");
                     word = strtok(NULL, " ");
-                    //printf("%s",word);
                     creat(word,  0644);  
-                    // printf("%s",strtok(cmdLine,"cat > ")); 
-                    //printf("%s", cmdLine);
                 }
-                else if(strcmp(word,"cat")==0)
+            else if(strcmp(word,"cat")==0)
                 {
-
-                    //char* word = strtok(cmdLine, " ");
                     word = strtok(NULL, " ");
                     execl("/bin/cat", "cat", word, NULL);
- 
             }
             else if(strcmp(word,"wc")==0){
                  word = strtok(NULL, " ");
@@ -80,14 +66,9 @@
             }
 
             else if(strcmp(word,"cp")==0){
-
-
                     word=strtok(NULL, " ");
                     strcpy(file1,word);
                     word=strtok(NULL, " ");
-                    //  printf("%s",nw);
-                    //  strcpy(file2,word);
-                    //file2=strtok(NULL, " ");
                     execl("/bin/cp", "cp", file1,word, NULL);
                 }
 
@@ -97,20 +78,14 @@
                     if (strcmp(word,"-r")==0){
                         word=strtok(NULL, " ");
                         execlp("sort","sort","-r", word,NULL);
-
                     }
-                    else
-                    {
+                    else{
                         execlp("sort","sort",word,NULL);
                     }
 
             }
 
-
-
-
-
-                else if(strcmp(word,"man")==0){
+            else if(strcmp(word,"man")==0){
                     word=strtok(NULL, " ");
                     if((strcmp(word,"pwd")==0)||(strcmp(word,"cd")==0)||(strcmp(word,"cat")==0)||(strcmp(word,"nano")==0)||(strcmp(word,"wc")==0)||(strcmp(word,"man")==0)||(strcmp(word,"cp")==0)||(strcmp(word,"sort")==0||(strcmp(word,"grep")==0)))
                         execlp("man","man", word,NULL);
